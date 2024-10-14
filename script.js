f366de30e614c3babb9909c24f350670f53d8bf1
Sebastian Gargosch <98969@student.vitkovickastredni.cz>
2024-10-14 09:35:22 +0200

    Up to ukol 3

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let first = document.getElementById('first');
 first.addEventListener('click', function() {
   alert("Kliknul si na tlačítko!");
 });
+
+const second_button = document.getElementById('second');
+
+second_button.addEventListener('click', function() {
+  second_button.textContent = "Text se změnil";
+});
+
+const third_button = document.getElementById('third');
+
+third_button.addEventListener('click', function() {
+  const third = document.querySelector('#change-text');
+  third.textContent = "Text se změnil";
+});
